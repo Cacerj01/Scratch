88672de9eceb266cab24f9414a275c362551549b
Javier Caceres <javier.caceres@pfizer.com>
2018-12-13 12:04:30 -0800

    Added the id handling module and the second temporary file

--- Python_lab/src/master_table.py
+++ Python_lab/src/master_table.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import glob
 import sys
+import re
 from os import mkdir
 from os import makedirs
 
@@ -35,19 +36,36 @@ except:
 try:
 	os.mkdir(args.output + '/Output')
 except:
-	print("ERROR: The ./Output folder could not be created or already exist. Check the output path.")
+	sys.exit("ERROR: The output folder could not be created or already exist. Check the output path.")
 
 # Entra en la carpeta de los bam y genera Control_Cambios_V1.txt
 cambiosv1_path = os.path.join(args.output, 'Output', 'Control_Cambios_V1.txt')
 cambiosv1 = open(cambiosv1_path, 'w')
-
-print("El temporal V1 se hizo en %s" % cambiosv1_path)
 
 os.chdir(args.input)
 for root, dirs, files in os.walk(args.input):
   for file in files:
     if file.endswith(".bam"):
       print(os.path.join(root, file), file=cambiosv1)
+cambiosv1.close()
 
-# 
+# Genera el archivo temporal Control_Cambios_V2.txt
+cambiosv2_path = os.path.join(args.output, 'Output', 'Control_Cambios_V2.txt')
+cambiosv2 = open(cambiosv2_path, 'w')
 
+f = open(cambiosv1_path, "r")
+line = f.readline()
+while line:
+    line = "".join(line.split("\n"))
+    line2 = line.replace("/","_")
+    id = line2.split("_")[-2]
+    if id is not None:
+        if len(id) != 8:
+            print(line + "\t" + "NEWID", file = cambiosv2)
+        elif id.isdigit() == False:
+            print(line + "\t" + "NEWID", file = cambiosv2)
+        else:
+            print(line + "\t" + id, file = cambiosv2)
+    line = f.readline()
+f.close()
+cambiosv2.close()
