37b65da4bd2b013e3c647648508d46c6f402faa0
Javier Caceres <javier.caceres@pfizer.com>
2018-12-13 06:09:38 -0800

    Updated the argument test module and temporal files handling

--- Python_lab/src/master_table.py
+++ Python_lab/src/master_table.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import os.path
 import glob
+import sys
 from os import mkdir
 from os import makedirs
 
@@ -13,34 +14,40 @@ path = os.getcwd()
 # Se listan los argumentos para la funcionalidad del script
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
 				description='Create a Master Table from Unprocessed genomic data')
-parser.add_argument('-i', '--input', metavar='', default=path, help='path to the root folder for unprocessed bam files. (default: ./)')
-parser.add_argument('-o', '--output', metavar='', default=path, help='path to the output folder for the processed bam files. (default: ./Output)' )
-parser.add_argument('-v', '--version', action='version', version='Master Table Creator v1.0 Dec. 2018')
+parser.add_argument('-i','--input', metavar='', default=path, help='path to the root folder for unprocessed bam files. (default: ./)')
+parser.add_argument('-o','--output', metavar='', default=path, help='path to the output folder for the processed bam files. (default: ./Output)' )
+parser.add_argument('-t','--temp-files', metavar='', default='F', help='choose if you want to keep the temp and intermediary files; useful for debugging. (default: F or False) ')
+parser.add_argument('-v','--version', action='version', version='Master Table Creator v1.0 Dec. 2018')
 args = parser.parse_args()
 
 # Revisa si los atributos entregados son validos
 try:
 	os.chdir(args.input)
 except:
-	print("ERROR: The input path provided is invalid ('%s')" % args.input)
+	sys.exit("ERROR: The input path provided does not exist. Check the route and try again.")
 
 try:
         os.chdir(args.output)
 except:
-        print("ERROR: The output path provided is invalid ('%s')" % args.output)
+	sys.exit("ERROR: The output path provided is invalid or do not have write permisions.")
 
 # Crea la carpeta de salida y revisa si existe
 try:
 	os.mkdir(args.output + '/Output')
 except:
-	print("ERROR: The ./Output folder could not be created. Check the output path.")
+	print("ERROR: The ./Output folder could not be created or already exist. Check the output path.")
 
 # Entra en la carpeta de los bam y genera Control_Cambios_V1.txt
-cambiosv1 = open('Control_Cambios_V1.txt', 'w')
+cambiosv1_path = os.path.join(args.output, 'Output', 'Control_Cambios_V1.txt')
+cambiosv1 = open(cambiosv1_path, 'w')
+
+print("El temporal V1 se hizo en %s" % cambiosv1_path)
 
 os.chdir(args.input)
 for root, dirs, files in os.walk(args.input):
   for file in files:
     if file.endswith(".bam"):
       print(os.path.join(root, file), file=cambiosv1)
+
+# 
 
